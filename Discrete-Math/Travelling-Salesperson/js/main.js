7e86b42a0d8bbd5aa2cb2462de805e6ce7ac6310
Rachel "Wil Sha" Morris <racheljmorris@gmail.com>
2017-11-20 21:02:06 -0600

    Added random path generator

--- Discrete-Math/Travelling-Salesperson/js/main.js
+++ Discrete-Math/Travelling-Salesperson/js/main.js
@@ -41,8 +41,12 @@ main = {
     },
 
     draw : function() {
+        var bgColor = "#8afc90";
+        var nodeColor = "#34086c";
+        var edgeLabelColor = nodeColor;
+        
         // Fill background
-        main.canvasWindow.fillStyle = "#ace9ff";
+        main.canvasWindow.fillStyle = bgColor;
         main.canvasWindow.fillRect( 0, 0, main.settings.width, main.settings.height );
 
         main.canvasWindow.font = "15px Arial";
@@ -50,12 +54,13 @@ main = {
         main.canvasWindow.fillText( "Travelling Salesperson", 10, 15 );
         main.canvasWindow.fillText( "Click two nodes to connect them.", 10, 30 );
         main.canvasWindow.fillText( "'C' clears the path.", 10, 45 );
+        main.canvasWindow.fillText( "'R' generates a random path.", 10, 60 );
         
         main.canvasWindow.font = "20px Arial";
         main.canvasWindow.fillText( "Total distance: " + main.distance, 10, 750 );
 
         main.canvasWindow.font = "15px Arial";
-        main.canvasWindow.fillStyle = "#0e7ca5";
+        main.canvasWindow.fillStyle = nodeColor;
         main.canvasWindow.fillText( "Programmed by Rachel Morris", main.settings.width - 220, main.settings.height - 15 );
 
         // Draw edges
@@ -66,14 +71,15 @@ main = {
             main.canvasWindow.moveTo( main.edges[i].startX, main.edges[i].startY );
             main.canvasWindow.lineTo( main.edges[i].endX, main.edges[i].endY );
             main.canvasWindow.stroke();
-            main.canvasWindow.font = "15px Arial";
+            main.canvasWindow.font = "20px Arial";
+            main.canvasWindow.fillStyle = edgeLabelColor;
             main.canvasWindow.fillText( main.edges[i].counter, (main.edges[i].startX + main.edges[i].endX) / 2, (main.edges[i].startY + main.edges[i].endY) / 2 );
         }
 
         // Draw nodes
         for ( var i = 0; i < main.nodes.length; i++ )
         {
-            main.canvasWindow.fillStyle = "#0e7ca5";
+            main.canvasWindow.fillStyle = nodeColor;
             main.canvasWindow.fillRect( main.nodes[i].x, main.nodes[i].y, 5, 5 );
             main.canvasWindow.font = "15px Arial";
             main.canvasWindow.fillText( main.nodes[i].name, main.nodes[i].x + 10, main.nodes[i].y + 10 );
@@ -89,6 +95,12 @@ main = {
             main.canvasWindow.arc( ctrX, ctrY, 15, 0, 2 * Math.PI );
             main.canvasWindow.stroke();
         }
+    },
+
+    clear: function() {
+        main.distance = 0;
+        main.edges.splice( 0, main.edges.length );
+        main.lastClick = -1;
     },
 
     // Events
@@ -129,13 +141,57 @@ main = {
         }
     },
 
+    generatePath : function( startNode ) {
+        main.clear();
+        var lastNode = startNode;
+        var newNode = 0;
+
+        var availableNodes = [ 0, 2, 3, 4, 5 ];
+
+        for ( var i = 0; i < 5; i++ )
+        {
+            var rand = Math.floor( Math.random() * ( availableNodes.length ) );
+            newNode = availableNodes[rand];
+
+            console.log( lastNode, "-", newNode );
+            
+            main.distance += main.getDistance( main.nodes[lastNode].x, main.nodes[lastNode].y, main.nodes[newNode].x, main.nodes[newNode].y );
+            var edge = {
+                startX : main.nodes[lastNode].x,
+                startY : main.nodes[lastNode].y,
+                endX : main.nodes[newNode].x,
+                endY : main.nodes[newNode].y,
+                counter : main.edges.length + 1
+            };
+            main.edges.push( edge );
+            lastNode = newNode;
+            availableNodes.splice( rand, 1 );
+        }
+
+        console.log( "Last:", lastNode, "-", newNode );
+
+        // add final path
+        newNode = startNode;
+        main.distance += main.getDistance( main.nodes[lastNode].x, main.nodes[lastNode].y, main.nodes[newNode].x, main.nodes[newNode].y );
+        var edge = {
+            startX : main.nodes[lastNode].x,
+            startY : main.nodes[lastNode].y,
+            endX : main.nodes[newNode].x,
+            endY : main.nodes[newNode].y,
+            counter : main.edges.length + 1
+        };
+        main.edges.push( edge );
+    },
+
     keydown : function( event )
     {
         if ( event.key == "c" )
         {
-            main.distance = 0;
-            main.edges.splice( 0, main.edges.length );
-            main.lastClick = -1;
+            main.clear();
+        }
+        else if ( event.key == "r" )
+        {
+            main.generatePath( 1 );
         }
     },
 
